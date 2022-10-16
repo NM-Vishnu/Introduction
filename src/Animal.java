51c2748620d42bbc50d8a4fe45bdf514fc5407f3
Vishnu <vishnuram2012@gmail.com>
2022-10-16 23:25:26 +0100

    Initial commit

---
+++ src/Animal.java
@@ -0,0 +1,29 @@
+/**
+ * This is a simple Java Bean Class
+ * @author Vishnu
+ * @version 11
+ */
+public class Animal {
+    private String name;
+
+    /**
+     * get name of animal
+     * @return name of animla
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * set name of animal
+     * @param name - name of animal
+     */
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome Program");
+    }
+}
